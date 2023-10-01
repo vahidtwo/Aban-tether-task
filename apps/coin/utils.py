fba2a48bd2cc117cd6f9c171c6b002ef3138fbd6
vahidtwo <vahidtwo@gmail.com>
2023-10-01 15:30:38 +0330

    :truck: move get_transfer_tax

--- apps/coin/utils.py
+++ apps/coin/utils.py
@@ -8,3 +8,10 @@ def get_coin_price_from_market(coin: Coin) -> Decimal:
     fetch price from market for a coin
     """
     return Decimal(4.0)
+
+
+def get_transfer_tax(coin: Coin, amount: Decimal) -> Decimal:
+    """
+    fetch transfer tax from market for a coin
+    """
+    return Decimal(0.001)
--- apps/exchange/utils.py
+++ apps/exchange/utils.py
@@ -4,13 +4,6 @@ from apps.coin.models import Coin
 from apps.exchange.integration.exchange_schema import ExchangeABC
 
 
-def get_transfer_tax(coin: Coin, amount: Decimal) -> Decimal:
-    """
-    fetch transfer tax from market for a coin
-    """
-    return Decimal(0.001)
-
-
 def buy_from_exchange(coin: Coin, amount: Decimal, exchange: ExchangeABC) -> Decimal:
     """
     buy currency from exchange for a coin
