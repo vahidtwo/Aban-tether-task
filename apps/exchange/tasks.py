107bdfdc896e49a2d07b65838fa2f744b3e2d63f
vahidtwo <vahidtwo@gmail.com>
2023-10-01 12:21:46 +0330

    :technologist: improve perform and reliability of completion order celery task

--- apps/exchange/tasks.py
+++ apps/exchange/tasks.py
@@ -1,3 +1,6 @@
+import time
+import uuid
+
 from celery import shared_task
 from django.db import transaction
 from django.db.models import Sum, Q
@@ -5,46 +8,51 @@ from rest_framework import status
 
 from apps.account.models import UserCryptoWallet
 from apps.coin.models import Coin
-from apps.exchange.exchanges import ExchangeManager
+from apps.exchange.integration.exchanges import ExchangeManager
 from apps.exchange.models import Order
+from config.celery import check_task_is_running
 
 
 @shared_task
-def try_to_complete_orders():
-    # TODO lock data and use one manager to check just one consumer run this task
+def try_to_complete_orders(coin_id: uuid.uuid4):
+    # TODO lock data and use better one manager to check just one consumer run compute_task task
     # or implement a watchdog service to check it
     # for limitation of time this implement like this
+    while check_task_is_running("apps.exchange.tasks.compute_task"):
+        time.sleep(0.5)
+    compute_task.deley(args=[coin_id])
+
+
+@shared_task
+def compute_task(coin_id: uuid.uuid4):
     """
     Complete orders accord to requested coin on orders
     the orders send to exchange when aggregate of order-price rich to 10$
     """
-    with transaction.atomic():
-        coins = Coin.objects.annotate(
-            orders_price=Sum(
-                "orders__total_price",
-                filter=~Q(
-                    order__status__in=[Order.OrderStatus.BUY_FROM_EXCHANGE, Order.OrderStatus.ADDED_TO_USER_WALLET]
-                ),
-            ).filter(
-                orders_price__gre=10.0  # TODO it must get limit of order from gateway of related exchange
-            )
-        )
-        for coin in coins.iterator():  # TODO it must use multy thread
-            orders = coin.orders.filter(
-                status__in=[Order.OrderStatus.BUY_FROM_EXCHANGE, Order.OrderStatus.ADDED_TO_USER_WALLET]
-            )
+    orders = (
+        Order.objects.filter(status__in=[Order.OrderStatus.BUY_FROM_EXCHANGE, Order.OrderStatus.ADDED_TO_USER_WALLET])
+        .annotate(sum_order_price=Sum("total_amount") - Sum("transfer_fee"))
+        .filter(sum_order_price__gte=10)  # TODO it must get limit of order from gateway of related exchange
+    )
+    if orders.exists():
+        with transaction.atomic():
+            coin = Coin.objects.get(id=coin_id)
+            orders = orders.select_for_update()
             orders.update(status=Order.OrderStatus.SEND_TO_EXCHANGE)
-            result = ExchangeManager(coin).buy(amount=coin.orders_price)
-            match result.status:
+            result = ExchangeManager(coin).buy(
+                amount=coin.orders_price
+            )  # TODO it must be async function that must await here
+            match result.status_code:
                 case status.HTTP_200_OK:
                     orders.update(status=Order.OrderStatus.BUY_FROM_EXCHANGE)
-                    add_complete_orders_to_user_wallets.delay()
                 case status.HTTP_429_TOO_MANY_REQUESTS:
                     # TODO use token buket algorithm
                     # check https://www.krakend.io/docs/throttling/token-bucket/
                     orders.update(status=Order.OrderStatus.RETRY_TO_SEND_TO_EXCHANGE)
                 case _:
                     orders.update(status=Order.OrderStatus.FAILED, failed_result=str(result))
+            if result.status_code == status.HTTP_200_OK:
+                add_complete_orders_to_user_wallets.delay()
 
 
 @shared_task
