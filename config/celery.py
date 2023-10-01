c9142eb159312340a2a695dd3d04b16bd61fa8aa
vahidtwo <vahidtwo@gmail.com>
2023-10-01 12:19:09 +0330

    :sparkles: add func for check a task is running

--- config/celery.py
+++ config/celery.py
@@ -16,3 +16,13 @@ app.autodiscover_tasks()
 # Load task modules from all registered Django app configs.
 packages = []
 app.autodiscover_tasks(packages=packages)
+
+
+def check_task_is_running(task_import_path: str) -> bool:
+    """check one task is running in celery or not"""
+    is_compute_task_is_running = False
+    for worker_name, tasks in app.control.inspect().active().items():
+        for task in tasks:
+            if task["name"] == task_import_path:
+                is_compute_task_is_running = True
+    return is_compute_task_is_running
