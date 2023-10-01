b35eb9559cabbe1a8ee3c08c44a8a9ce2e2ce36c
vahidtwo <vahidtwo@gmail.com>
2023-10-01 12:18:10 +0330

    :bug: fix celery deprecation config - add django-redis to requirement

--- config/celery.py
+++ config/celery.py
@@ -7,12 +7,12 @@ from celery import Celery
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.production")
 
 app = Celery("aban_tether")
-app.config_from_object("django.conf:lib_settings", namespace="CELERY")
+app.config_from_object("config.lib_settings.celery", namespace="CELERY")
 app.conf.timezone = "Asia/Tehran"
 
 app.autodiscover_tasks()
 
 
 # Load task modules from all registered Django app configs.
-packages = []
+packages = ["apps.exchange"]
 app.autodiscover_tasks(packages=packages)
--- config/lib_settings/celery.py
+++ config/lib_settings/celery.py
@@ -2,11 +2,7 @@ from config import env
 
 # https://docs.celeryproject.org/en/stable/userguide/configuration.html
 # celery Conf
-CELERY_BROKER_URL = f"redis://{env('REDIS_HOST')}/2"
-CELERY_RESULT_BACKEND = f"redis://{env('REDIS_HOST')}"
-CELERY_ACCEPT_CONTENT = ["json"]
-CELERY_TASK_SERIALIZER = "json"
-
-CELERY_TASK_SOFT_TIME_LIMIT = 20  # seconds
-CELERY_TASK_TIME_LIMIT = 30  # seconds
+CELERY_BROKER_URL = f"redis://{env('REDIS_HOST')}:6379/2"
+result_backend = f"redis://{env('REDIS_HOST')}:6379/1"
+CELERY_TASK_TRACK_STARTED = True
 CELERY_TASK_MAX_RETRIES = 3
