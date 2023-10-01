d89807918762a72c50b12af76a701a6cba484cac
vahidtwo <vahidtwo@gmail.com>
2023-10-01 12:22:56 +0330

    :sparkles: add api for create new order for a user

--- apps/exchange/views.py
+++ apps/exchange/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.db import transaction
+from rest_framework import status
+from rest_framework.generics import GenericAPIView
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from apps.coin.models import Coin
+from apps.exchange.serializers import OrderCreateSerializer
+from core.http import Response
+from .tasks import try_to_complete_orders
+from ..account.models import UserWallet
+
+
+class OrderView(GenericAPIView):
+    serializer_class = OrderCreateSerializer
+    permission_classes = [IsAuthenticated]
+
+    @transaction.atomic
+    def post(self, request, *args, **kwargs):
+        """
+        create an order for a coin
+        """
+        serializer = OrderCreateSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        coin: Coin
+        coin = serializer.validated_data["coin"]
+        UserWallet.withdraw(id=request.user.wallet.id, value=serializer.validated_data["amount"])
+        serializer.save(user=request.user, coin_price=coin.get_price(), exchange=coin.exchange)
+
+        try_to_complete_orders.delay(args=[coin.id])
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
