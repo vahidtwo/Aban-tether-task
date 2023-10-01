c29343b69a0669b600bfc39678b49082fb02b831
vahidtwo <vahidtwo@gmail.com>
2023-10-01 15:31:43 +0330

    :sparkles: persistAuthorization on swagger

--- config/lib_settings/swagger.py
+++ config/lib_settings/swagger.py
@@ -2,4 +2,7 @@ SPECTACULAR_SETTINGS = {
     "TITLE": "aban Tether API",
     "VERSION": "1.0.0",
     "SERVE_INCLUDE_SCHEMA": False,
+    "SWAGGER_UI_CONFIG": """
+        persistAuthorization: true,
+    """,
 }
